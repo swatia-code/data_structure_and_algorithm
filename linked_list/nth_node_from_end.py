203a90bc018c3782a41f86c0e2f2689c07c91242
Swati Arora <swatia@juniper.net>
2020-07-05 17:53:42 +0530

    5 July 2020

---
+++ linked_list/nth_node_from_end.py
@@ -0,0 +1,139 @@
+'''
+PROBLEM STATEMENT
+-----------------
+Given a linked list consisting of L nodes and given a number N. The task is to find the Nth node from the end of the linked list.
+
+Input:
+The first line of input contains the number of testcase T. For each testcase, the first line of input contains the number of nodes in the linked list and the number N. The next line contains L nodes of the linked list.
+
+Output:
+For each testcase, output the data of node which is at Nth distance from the end or -1 in case node doesn't exist.
+
+User Task:
+The task is to complete the function getNthFromLast() which takes two arguments: reference to head and N and you need to return Nth from the end.
+
+Note:
+Try to solve in single traversal.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= T <= 200
+1 <= L <= 103
+1 <= N <= 103
+
+Example:
+Input:
+2
+9 2
+1 2 3 4 5 6 7 8 9
+4 5
+10 5 100 5
+Output:
+8
+-1
+
+Explanation:
+Testcase 1: In the first example, there are 9 nodes in linked list and we need to find 2nd node from end. 2nd node from end os 8.  
+Testcase 2: In the second example, there are 4 nodes in the linked list and we need to find 5th from the end. Since 'n' is more than the number of nodes in the linked list, the output is -1.
+
+LOGIC
+-----
+slow pointer fast pointer approach solves it;)
+
+SOURCE
+------
+geeksforgeeks
+
+CODE
+----
+'''
+'''
+	Your task is to return the data stored in
+	the nth end from end of linked list.
+	
+	Function Arguments: head (reference to head of the list), n (pos of node from end)
+	Return Type: Integer or -1 if no such node exits.
+
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+'''
+def getNthfromEnd(head,n):
+    #code here
+    if head == None:
+        return -1
+        
+    k = 0
+    fast_pointer = head
+    slow_pointer = head
+    
+    while k < n - 1:
+        fast_pointer = fast_pointer.next
+        if fast_pointer == None:
+            return -1
+        k += 1
+            
+    while fast_pointer.next != None:
+        fast_pointer = fast_pointer.next
+        slow_pointer = slow_pointer.next
+    
+    return slow_pointer.data
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+#Contributed by : Nagendra Jha
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+    
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        self.tail = new_node
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n,nth_node = map(int, input().strip().split())
+        a = LinkedList() # create a new linked list 'a'.
+        nodes_a = list(map(int, input().strip().split()))
+        for x in nodes_a:
+            a.append(x)  # add to the end of the list
+        print(getNthfromEnd(a.head,nth_node))
+# } Driver Code Ends
+
