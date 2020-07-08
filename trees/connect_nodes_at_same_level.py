c9ef484c28bc19692e7d8cf776badda90c6f0637
Swati Arora <swatia@juniper.net>
2020-07-08 18:03:34 +0530

    8 July 2020

---
+++ trees/connect_nodes_at_same_level.py
@@ -0,0 +1,215 @@
+'''
+PROBLEM STATEMENT
+-----------------
+Given a binary tree, connect the nodes that are at same level. You'll be given an addition nextRight pointer for the same.
+
+Initially, all the nextRight pointers point to garbage values. Your function should set these pointers to point next right for each node.
+       10                       10 ------> NULL
+      / \                       /      \
+     3   5       =>     3 ------> 5 --------> NULL
+    / \     \               /  \           \
+   4   1   2          4 --> 1 -----> 2 -------> NULL
+
+Input:
+First line of input contains the number of test cases T. For each test case, there will be only a single line of input which is a string representing the tree as described below: 
+
+The values in the string are in the order of level order traversal of the tree where, numbers denotes node values, and a character “N” denotes NULL child.
+
+For example:
+
+For the above tree, the string will be: 1 2 3 N N 4 6 N 5 N N 7 N
+
+Output:
+For each testcase, in a new line, print the level order and inorder traversals.
+
+Your Task:
+You don't have to take input. Complete the function connect() that takes root as parameter and connects the nodes at same level. The printing is done by the driver code.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 <= T <= 100
+1 <= Number of nodes <= 100
+1 <= Data of a node <= 1000
+
+Example:
+Input:
+2
+3 1 2
+10 20 30 40 60
+Output:
+3 1 2
+1 3 2
+10 20 30 40 60
+40 20 60 10 30
+
+Explanation:
+Testcase1: The connected tree is
+        3 ------> NULL
+     /      \
+   1 -----> 2 ------ NULL
+Testcase2: The connected tree is
+                           10 ----------> NULL
+                        /        \
+                     20 ------> 30 -------> NULL
+                  /       \
+               40 ----> 60 ----------> NULL
+ 
+
+LOGIC
+-----
+Level Order Traversal with NULL markers which are needed to mark levels in tree.
+
+SOURCE
+------
+geeksforgeeks
+
+CODE
+----
+'''
+def connect(root):
+    '''
+    :param root: root of the given tree
+    :return: none, just connect accordingly.
+    {
+        # Node Class:
+        class Node:
+            def __init__(self,val):
+                self.data = val
+                self.left = None
+                self.right = None
+                self.nextRight = None
+    }
+    '''
+    if root == None:
+        return 
+    
+    queue = list()
+    queue.append(root)
+    queue.append(None)
+    
+    while len(queue):
+        
+        p = queue.pop(0)
+        
+        if p:
+            p.nextRight = queue[0]
+            if p.left:
+                queue.append(p.left)
+            if p.right:
+                queue.append(p.right)
+                
+        elif queue:
+            queue.append(None)
+
+import sys
+sys.setrecursionlimit(50000)
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+        self.nextRight = None
+    
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+
+
+def InOrder(root):
+    '''
+    :param root: root of the given tree.
+    :return: None, print the space separated in order Traversal of the given tree.
+    '''
+    if root is None: # check if the root is none
+        return
+    InOrder(root.left) # do in order of left child
+    print(root.data, end=" ")  # print root of the given tree
+    InOrder(root.right) # do in order of right child
+
+def printSpecial(root):
+    leftmost_node = root
+
+    while leftmost_node :
+        curr_node = leftmost_node
+        leftmost_node = None
+        if curr_node.left :
+            leftmost_node = curr_node.left
+        elif curr_node.right :
+            leftmost_node = curr_node.right
+
+        print(curr_node.data,end=" ")
+        while curr_node.nextRight :
+            print(curr_node.nextRight.data,end=" ")
+            curr_node = curr_node.nextRight
+    print()
+
+
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        connect(root)
+        printSpecial(root)
+        InOrder(root)
+        print()
+
