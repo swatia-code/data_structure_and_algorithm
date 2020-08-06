7370dbe84ed0ec27c94b32baf7bc78dda6e190d9
Swati Arora <swatia@juniper.net>
2020-08-06 23:25:21 +0530

    6 August 2020

---
+++ linked_list/check_if_linked_list_is_palindrome.py
@@ -0,0 +1,101 @@
+'''
+PROBLEM STATEMENT
+-----------------
+Given a singly linked list of size N of integers. The task is to check if the given linked list is palindrome or not.
+
+Example 1:
+
+Input:
+N = 3
+value[] = {1,2,1}
+Output: 1
+Explanation: The given linked list is
+1 2 1 , which is a palindrome and
+Hence, the output is 1.
+Example 2:
+
+Input:
+N = 4
+value[] = {1,2,3,4}
+Output: 0
+Explanation: The given linked list
+is 1 2 3 4 , which is not a palindrome
+and Hence, the output is 0.
+Your Task:
+The task is to complete the function isPalindrome() which takes head as reference as the only parameter and returns true or false if linked list is palindrome or not respectively.
+
+Expected Time Complexity: O(N)
+Expected Auxialliary Space Usage: O(1)  (ie, you should not use the recursive stack space as well)
+
+Constraints:
+1 <= N <= 50
+
+LOGIC
+-----
+A simple solution is to use a stack of list nodes. This mainly involves three steps.
+Traverse the given list from head to tail and push every visited node to stack.
+Traverse the list again. For every visited node, pop a node from stack and compare data of popped node with currently visited node.
+If all nodes matched, then return true, else false.
+
+SOURCE
+------
+geeksforgeeks
+
+CODE
+----
+'''
+def isPalindrome(head):
+    #code here
+    lis = list()
+    temp = head
+    while temp:
+        lis.append(temp.data)
+        temp = temp.next
+    
+       
+    while head:
+        if head.data != lis[-1]:
+            return False
+        lis.pop(-1)
+        head = head.next
+        
+    return True
+
+import atexit
+import io
+import sys
+
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        self.tail = new_node 
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n = int(input())
+        a = LinkedList() # create a new linked list 'a'.
+        nodes_a = list(map(int, input().strip().split()))
+        for x in nodes_a:
+            a.append(x)  # add to the end of the list
+
+        if isPalindrome(a.head):
+            print(1)
+        else:
+            print(0)
