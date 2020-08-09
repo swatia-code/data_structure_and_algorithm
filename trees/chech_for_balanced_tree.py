e22bc5d97283631e2fa25d7b03f5487ae93f107d
Swati Arora <swatia@juniper.net>
2020-08-09 21:33:17 +0530

    9 August 2020

---
+++ trees/chech_for_balanced_tree.py
@@ -0,0 +1,175 @@
+'''
+PROBLEM STATEMENT
+-----------------
+Given a binary tree, find if it is height balanced or not. 
+A tree is height balanced if difference between heights of left and right subtrees is not more than one for all nodes of tree. 
+
+A height balanced tree
+        1
+     /     \
+   10      39
+  /
+5
+
+An unbalanced tree
+        1
+     /    
+   10   
+  /
+5
+
+Example 1:
+
+Input:
+      1
+    /
+   2
+    \
+     3 
+Output: 0
+Explanation: The max difference in height
+of left subtree and right subtree is 2,
+which is greater than 1. Hence unbalanced
+Example 2:
+
+Input:
+       10
+     /   \
+    20   30 
+  /   \
+ 40   60
+Output: 1
+Explanation: The max difference in height
+of left subtree and right subtree is 1.
+Hence balanced. 
+Your Task:
+You don't need to take input. Just complete the function isBalanced() that takes root node as parameter and returns true, if the tree is balanced else returns false.
+
+Constraints:
+1 <= Number of nodes <= 105
+0 <= Data of a node <= 106
+
+Expected time complexity: O(N)
+Expected auxiliary space: O(h) , where h = height of tree
+
+
+LOGIC
+-----
+An empty tree is height-balanced. A non-empty binary tree T is balanced if:
+1) Left subtree of T is balanced
+2) Right subtree of T is balanced
+3) The difference between heights of left subtree and right subtree is not more than 1.
+
+SOURCE
+------
+geekforgeeks
+
+CODE
+----
+'''
+def height(root):
+    if root is None:
+        return 0
+        
+    left_height = height(root.left)
+    right_height = height(root.right)
+    
+    return max(left_height, right_height) + 1
+    
+
+def isBalanced(root):
+    
+    #add code here
+    if root is None:
+        return True
+        
+    left_height = height(root.left)
+    right_height = height(root.right)
+    
+    if (abs(left_height - right_height) <= 1) and isBalanced(root.left) and isBalanced(root.right):
+        return True
+    
+    return False
+
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        if isBalanced(root):
+            print(1) 
+        else:
+            print(0)
