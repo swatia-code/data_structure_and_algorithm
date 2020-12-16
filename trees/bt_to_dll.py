06ad990681208644f3903855d152b5479b14c4e1
Swati Arora <swatia@juniper.net>
2020-12-16 19:03:07 +0530

    16 December 2020

---
+++ trees/bt_to_dll.py
@@ -0,0 +1,171 @@
+'''
+PROBLEM STATEMENT
+-----------------
+Given a Binary Tree (BT), convert it to a Doubly Linked List(DLL) In-Place. The left and right pointers in nodes are to be used as previous and next pointers respectively in converted DLL. The order of nodes in DLL must be same as Inorder of the given Binary Tree. The first node of Inorder traversal (leftmost node in BT) must be the head node of the DLL.
+
+TreeToList
+
+Example 1:
+
+Input:
+      1
+    /  \
+   3    2
+Output:
+3 1 2 
+2 1 3 
+Explanation: DLL would be 3<=>1<=>2
+Example 2:
+
+Input:
+       10
+      /   \
+     20   30
+   /   \
+  40   60
+Output:
+40 20 60 10 30 
+30 10 60 20 40
+Explanation:  DLL would be 
+40<=>20<=>60<=>10<=>30.
+Your Task:
+You don't have to take input. Complete the function bToDLL() that takes root node of the tree as a parameter and returns the head of DLL . The driver code prints the DLL both ways.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(H).
+Note: H is the height of the tree and this space is used implicitly for recursion stack.
+
+Constraints:
+1 <= Number of nodes <= 1000
+1 <= Data of a node <= 1000
+
+LOGIC
+-----
+
+SOURCE
+------
+geeksforgeeks
+
+CODE
+----
+'''
+#User function Template for python3
+
+'''
+class Node:
+    """ Class Node """
+    def __init__(self, value):
+        self.left = None
+        self.data = value
+        self.right = None
+'''
+
+def btodll(root,prev):
+    if root==None:
+        return
+    btodll(root.left,prev)
+    if prev[0]!=None:
+        root.left=prev[0]
+        prev[0].right=root
+    prev[0]=root
+    btodll(root.right,prev)
+def bToDLL(root):
+    prev=[None]
+    btodll(root,prev)
+    curr=prev[0]
+    while curr.left!=None:
+        curr=curr.left
+    return curr
+
+
+
+#{ 
+#  Driver Code Starts
+from collections import deque
+class Node:
+    """ Class Node """
+    def __init__(self, value):
+        self.left = None
+        self.data = value
+        self.right = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+
+
+import sys            
+def printDLL(head): #Print util function to print Linked List
+    prev = None
+    sys.stdout.flush()
+    while(head != None):
+        print(head.data, end=" ")
+        prev=head
+        head=head.right
+    print('')
+    while(prev != None):
+        print(prev.data, end=" ")
+        prev=prev.left
+    print('')
+    
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        head = bToDLL(root)
+        printDLL(head)
+# } Driver Code Ends
